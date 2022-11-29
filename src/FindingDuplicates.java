4a804f4ea09e7747d324d27268368d496e8667fe
vajagani dhanesh <vajaganidhanesh@gmail.com>
2022-11-29 19:48:09 +0530

    FindingDuplicates numbers in an array

---
+++ src/FindingDuplicates.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class FindingDuplicates {
+    public static void main(String[] args) {
+    int arr[] = {1,4,3,4,2};
+        cyclic(arr);
+        System.out.println(cyclic(arr));
+    }
+    static int cyclic(int arr[]){
+        int i=0;
+
+        while(i<arr.length){
+            if(arr[i]!= i+1){
+                int correct = arr[i]-1;
+                if(arr[i]!=arr[correct]){
+                    swap(arr,i,correct);
+                }
+                else{
+                    return arr[i];
+                }
+            }
+            else{
+                i++;
+            }
+        }
+        return -1;
+    }
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
